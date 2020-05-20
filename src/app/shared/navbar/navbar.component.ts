e23eed4136d6d638d9027ad9ff27715de3447260
Pablo Diaz <pablo.diaz2799@gmail.com>
2020-05-20 15:59:16 -0300

    added about page, made it responsive, added responsive navbar, added semi-responsive contact page, missing new images, missing, galleries

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 import { ROUTES } from './navbar-routes.config';
+import {NavigationEnd, Router} from "@angular/router";
 
 
 @Component({
@@ -8,10 +9,44 @@ import { ROUTES } from './navbar-routes.config';
   styleUrls: ['./navbar.component.css']
 })
 export class NavBarComponent implements OnInit {
+  url = '';
+  constructor(router:Router) {
+    router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        setTimeout(() => {
+          this.hideMenu();
+          this.url = event.url;
+        },700);
+      }
+
+    });
+  }
+
+
   public menuItems: any [];
+  @ViewChild('sidebarMenu') menu: ElementRef;
+  @ViewChild('menuIcon') icon: ElementRef;
+  @Output() someEvent = new EventEmitter();
 
   ngOnInit(): void {
     this.menuItems = ROUTES;
+  }
+
+  showMenu(){
+    if(this.url == '/about'){
+      this.menu.nativeElement.style.width = '100%';
+      this.menu.nativeElement.style.right = '0%';
+    }
+    else{
+      this.menu.nativeElement.style.right = '0px';
+    }
+    this.menu.nativeElement.style.height = '100vh';
+    document.body.style.position = 'fixed';
+  }
+
+  hideMenu(){
+    this.menu.nativeElement.style.height = '0vh';
+    document.body.style.position = 'static';
   }
 
 }
