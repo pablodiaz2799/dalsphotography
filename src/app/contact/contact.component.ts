f98e7e6729c716ea5ef504367188e12860d81f3e
Pablo Diaz <pablo.diaz2799@gmail.com>
2020-06-02 16:54:48 -0300

    changing computers

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -10,6 +10,11 @@ export class ContactComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    event.target.innerWidth;
   }
 
 }
